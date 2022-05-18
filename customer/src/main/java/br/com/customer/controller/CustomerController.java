cd451cb6baae5ec3d69f031eb2a2d96040f1733b
Joaldo Santos <joaldo.einstein@hotmail.com>
2022-05-18 11:15:11 -0300

    Implementação do modulo Customer concluído

--- customer/src/main/java/br/com/customer/controller/CustomerController.java
+++ customer/src/main/java/br/com/customer/controller/CustomerController.java
@@ -1,9 +1,25 @@
 package br.com.customer.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import br.com.customer.controller.request.CustomerRequest;
+import br.com.customer.service.CustomerService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.*;
 
+@Slf4j
 @RestController
-@RequestMapping
+@RequestMapping("/api/v1/customers")
 public class CustomerController {
+
+    private final CustomerService customerService;
+
+    public CustomerController(CustomerService customerService) {
+        this.customerService = customerService;
+
+    }
+
+    @PostMapping
+    public void createCustomer(@RequestBody CustomerRequest customerRequest){
+        log.info("calling controller to create customer {}", customerRequest);
+        this.customerService.createCustomer(customerRequest);
+    }
 }
