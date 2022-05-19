ec324c17e9491b97196480d0b5e7a1f90cbf5d5a
Joaldo Santos <joaldo.einstein@hotmail.com>
2022-05-19 10:18:52 -0300

    Modulo customer com sumario da API

--- customer/src/main/java/br/com/customer/controller/CustomerController.java
+++ customer/src/main/java/br/com/customer/controller/CustomerController.java
@@ -2,7 +2,11 @@ package br.com.customer.controller;
 
 import br.com.customer.controller.request.CustomerRequest;
 import br.com.customer.service.CustomerService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @Slf4j
@@ -17,9 +21,12 @@ public class CustomerController {
 
     }
 
+    @Operation(summary = "create customer", description = "create customer to fraud system")
+    @ApiResponse(responseCode = "201", description = "Customer success created")
     @PostMapping
-    public void createCustomer(@RequestBody CustomerRequest customerRequest){
+    public ResponseEntity<?> createCustomer(@RequestBody CustomerRequest customerRequest){
         log.info("calling controller to create customer {}", customerRequest);
         this.customerService.createCustomer(customerRequest);
+        return ResponseEntity.status(HttpStatus.CREATED).build();
     }
 }
